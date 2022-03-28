ec639079b117b9756dad915d39f975f4ccb8f0d5
lejulien-42 <lejulien@students.42.fr>
2022-03-28 11:25:37 +0200

    progress

---
+++ frontend/src/player.ts
@@ -0,0 +1,8 @@
+/// <reference path="../global.d.ts" />
+
+function Player() {
+    this.w = 15;
+    this.h = 80;
+
+    this.pos = p5.createVector()
+}
--- frontend/src/sketch.ts
+++ frontend/src/sketch.ts
@@ -1,45 +1,107 @@
 /// <reference path="../global.d.ts" />
 
-let bally = 1;
-let ballx = 1;
-let Velocity = 6
-let balldx = Velocity;
-let balldy = Velocity;
+import { textSpanIntersectsWith } from "typescript";
+import { threadId } from "worker_threads";
+
 let WIDTH = 0;
 let HEIGHT = 0;
-let ballSiz = 0;
+let sepSize = 0;
+
+
+var Player = function(p) {
+  this.w = 15;
+  this.h = 80;
+
+  this.pos = p.createVector(this.w * 2, HEIGHT / 2 - this.h / 2);
+  this.acc = p.createVector(0, 0);
+  this.spd = 10;
+  this.maxSpd = 10;
+
+  this.show = function() {
+    p.noStroke();
+    p.fill(255);
+    p.rect(this.pos.x, this.pos.y, this.w, this.h);
+  }
+
+  this.update = function() {
+    this.acc.y = p.constrain(this.acc.y, -this.maxSpd, this.maxSpd);
+    this.pos.add(this.acc);
+    this.pos.y = p.constrain(this.pos.y, 0, HEIGHT - this.h);
+  }
+
+  this.up = function() {this.acc.y -= this.spd;}
+  this.down = function() {this.acc.y += this.spd;}
+  this.stop = function() {this.acc.y = 0;}
+}
+
+var Ball = function(p) {
+  this.x = WIDTH / 2;
+  this.y = HEIGHT / 2;
+  this.size = WIDTH * 0.02;
+  this.velocity = 9;
+  this.dx = this.velocity;
+  this.dy = this.velocity;
+
+  this.show = function() {
+    p.fill(200, 10, 200);
+    p.rect(this.x, this.y, this.size, this.size);
+  }
+
+  this.update = function() {
+    if (this.x <= 0 || (this.x + this.size > WIDTH))
+        this.dx = this.dx * -1;
+    if (this.y <= 0 || (this.y + this.size > HEIGHT))
+        this.dy = this.dy * -1;
+    this.x = this.x + this.dx;
+    this.y = this.y + this.dy;
+  }
+}
 
 var sketch = function (p: p5) {
+  let player;
+  let ball;
 
   p.setup = function () {
     WIDTH = p.windowWidth - 20;
-    HEIGHT = p.windowHeight / 2;
-    ballSiz = WIDTH * 0.02;
+    HEIGHT = p.windowHeight - 20;
+    sepSize = HEIGHT * 0.5 - 1;
     p.createCanvas(WIDTH , HEIGHT);
-    p.frameRate(30);
+    player = new Player(p);
+    ball = new Ball(p);
   }
 
   p.windowResized = function() {
     WIDTH = p.windowWidth - 20;
-    HEIGHT = p.windowHeight / 2;
-    ballSiz = WIDTH * 0.02;
+    HEIGHT = p.windowHeight - 20;
     p.resizeCanvas(WIDTH, HEIGHT);
   }
 
   p.draw = function () {
     p.background(0);
+    p.noStroke();
     p.fill(255);
-    p.rect(WIDTH / 2 -ballSiz, ballSiz, ballSiz, HEIGHT - 2 * ballSiz);
-    
-    if (ballx <= 0 || (ballx + ballSiz > WIDTH))
-        balldx = balldx * -1;
-    if (bally <= 0 || (bally + ballSiz > HEIGHT))
-        balldy = balldy * -1;
-    ballx = ballx + balldx;
-    bally = bally + balldy;
+    player.update();
+    ball.update();
+    player.show();
+    ball.show();
+  }
 
-    p.fill(200, 10, 200);
-    p.rect(ballx, bally, ballSiz, ballSiz);
+  p.keyPressed = function () {
+    if (p.key == "w" || p.key == "W" || p.keyCode == p.UP_ARROW) {
+      player.up();
+    }
+    else if (p.key == "s" || p.key == "S" || p.keyCode == p.DOWN_ARROW) {
+      player.down();
+    }
+  }
+
+  p.keyReleased = function () {
+    if (p.key == "w" || p.keyCode == p.UP_ARROW) {
+      player.stop();
+    }
+    else if (p.key == "s" || p.keyCode == p.DOWN_ARROW) {
+      player.stop();
+    }
   }
 }
 
